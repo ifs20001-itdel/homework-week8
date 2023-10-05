c65c56bde228a71b55a656dae04ab7e0545f6e71
ifs20001-itdel <ifs20001@students.del.ac.id>
2023-10-06 02:07:12 +0700

    Commit Homework Week 8

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ const testConnect = require('./router/testConnect')
 const film = require('./router/film')
 const listCategory = require('./router/listCategory')
 const actor = require('./router/actor')
-const filmCategory = require('./router/categoryFilm')
+const filmCategory = require('./router/filmCategory')
 
 app.use(testConnect)
 
-app.use('/film', film)
-
+// List file film.js di router/film.js untuk melihat Path yang digunakan untuk mengakses
 app.use(film)
 
+// Menampilkan data list Category
 app.use('/listCategory', listCategory)
 
+// Menampilkan data list Film berdasarkan Category
 app.use(filmCategory)
 
+// Menampilkan data list Table Actor
 app.use('/actor', actor)
 
 app.listen(port, ()=>{
--- router/film.js
+++ router/film.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const pool = require('../queries')
 
-router.get('/', (req, res)=>{
+// Menampilkan data seluruh list film
+router.get('/listFilm', (req, res)=>{
     const sql = 'SELECT * FROM film'
     pool.query(sql, (error, result)=>{
         if(error) throw error
@@ -10,6 +11,7 @@ router.get('/', (req, res)=>{
     })
 })
 
+// Menampilkan data film tertentu berdasarkan id
 router.get('/listFilm/:id', (req, res)=>{
     const film_id = req.params.id
     const sql = 'SELECT * FROM film WHERE film_id=$1'
